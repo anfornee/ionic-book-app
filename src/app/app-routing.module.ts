808acdcdbbc17824020010da85837d0b58b20784
Anthony Pereira <anthonyp@ichop.org>
2019-06-17 15:23:32 -0700

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'one', loadChildren: './one/one.module#OnePageModule' },
+  { path: 'two', loadChildren: './two/two.module#TwoPageModule' },
+  { path: 'three', loadChildren: './three/three.module#ThreePageModule' },
+  { path: 'toc', loadChildren: './toc/toc.module#TOCPageModule' },
+  { path: 'page-two', loadChildren: './page-two/page-two.module#PageTwoPageModule' },
+  { path: 'page-three', loadChildren: './page-three/page-three.module#PageThreePageModule' },
+  { path: 'page-five', loadChildren: './page-five/page-five.module#PageFivePageModule' },
+  { path: 'page-seven', loadChildren: './page-seven/page-seven.module#PageSevenPageModule' },
 ];
 
 @NgModule({
